e91e1ae204cde284687fe4db2654e2d85cf25f8f
Hhellobro <154648634+Hhellobro@users.noreply.github.com>
2023-12-26 17:47:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask 
 
-app = Flask(name) 
+app = Flask(__name__) 
 
 @app.route('/') 
 
@@ -8,5 +8,5 @@ def hello_World() :
 
   return 'AlphaDevloper'
 
-if name == "main":
+if __name__ == "__main__":
   app.run() 
