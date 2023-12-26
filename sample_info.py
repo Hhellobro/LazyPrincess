626e89cfe1a6c805db91fa4f80fe435f13871f48
Hhellobro <154648634+Hhellobro@users.noreply.github.com>
2023-12-26 10:45:32 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,10 +1,10 @@
-'''
+
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 20813113
+API_HASH = '431fb2a573604b219de19277a0d08b6c'
+BOT_TOKEN = '6718834728:AAHGjt3DU2PFyqtGMPUmYC6_08BTVEL2h4k'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -12,15 +12,15 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [6718834728]
+CHANNELS = [-1002023004406, 'channelusername']
 AUTH_USERS = []
-AUTH_CHANNEL = None
+AUTH_CHANNEL = None 
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://hellobro:Su1&7@9$2?11@cluster0.gtgqxwe.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 
-'''
+
