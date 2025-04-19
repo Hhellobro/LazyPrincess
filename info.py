4f89c60f72c357038bb93904611781b555d31523
Hhellobro <154648634+Hhellobro@users.noreply.github.com>
2025-04-19 16:34:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,9 +14,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'AlphaMovieBOT')
-API_ID = int(environ.get('API_ID','27920615')) 
-API_HASH = environ.get('API_HASH','fca37d30df0f1d0846d91308f8569a0b') 
-BOT_TOKEN = environ.get('BOT_TOKEN','6741315301:AAEZPQKEIAU_Tjvhwk6ZBNR79DzPf11ZJJE') 
+API_ID = int(environ.get('API_ID','')) 
+API_HASH = environ.get('API_HASH','') 
+BOT_TOKEN = environ.get('BOT_TOKEN','') 
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
